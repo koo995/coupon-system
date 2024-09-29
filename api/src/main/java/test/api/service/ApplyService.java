ab8fa0632deb759363906c4ddc420de8002e6ef3
koo995 <107671886+koo995@users.noreply.github.com>
2024-09-30 00:47:43 +0900

    레디스를 incr 활용해서 문제 해결하기
    문제가 없어보일 수 있을지라도... 쿠폰의 갯수가 많아질수록 rdb에 부하가 주게된다.
    만약 사용하는 rdb가 쿠폰전용 db가 아니라 다양한 곳에서 사용하고 있었다면 다른 서비스까지 장애가 발생할 수 있다.

--- api/src/main/java/test/api/service/ApplyService.java
+++ api/src/main/java/test/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package test.api.service;
 
 import org.springframework.stereotype.Service;
 import test.api.domain.Coupon;
+import test.api.repository.CouponCountRepository;
 import test.api.repository.CouponRepository;
 
 @Service
@@ -9,16 +10,18 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     /**
      * 쿠폰 발급 로직
      */
     public void apply(Long userId) {
-        long count = couponRepository.count();
-
+        Long count = couponCountRepository.increment();
         if (count > 100) {
             return;
         }
--- api/src/test/java/test/api/service/ApplyServiceTest.java
+++ api/src/test/java/test/api/service/ApplyServiceTest.java
@@ -34,6 +34,16 @@ class ApplyServiceTest {
         assertThat(count).isEqualTo(1);
     }
 
+    /**
+     * 레이스 컨디션을 해결하기 위해서 제일 먼저,
+     * 1. Java 에서 지원하는 synchronized 키워드를 사용할 수 있습니다. 하지만 서버가 여러대라면 동기화가 되지 않습니다.
+     * 2. 다른 방법으로는 mysql을 활용한 락이 있다. 하지만 쿠폰개수를 가져오는 것부터 쿠폰을 생성하는 구간까지 락을 걸어야 해서 락을 거는 기간이 길어진다.
+     * 이는 곧, 성능이 느려지는 문제가 있다.
+     * 이 프로젝트의 핵심은 쿠폰의 갯수이므로 쿠폰 갯수의 정합성을 관리하면 된다.
+     * 3. 레디스에는 incr이라는 명령어가 있고 이는 키의 벨류를 1씩 증가시킨다.
+     * 레디스는 싱글 스레드를 기반으로 동작하여 레이스 건디션을 해결할 수 있고, incr은 성능도 굉장히 빠르다.
+     * 이 명령어를 사용하여 발급된 쿠폰 갯수를 제어한다면 성능도 빠르며 데이터 장합성도 지킬 수 있다. 이것이 Redis 을 사용하는 이유다.
+     */
     @DisplayName("동시에 여러명 응모")
     @Test
     void 여러_응모() throws Exception {
@@ -52,7 +62,6 @@ class ApplyServiceTest {
                 }
             });
         }
-
         latch.await();
 
         long count = couponRepository.count();
