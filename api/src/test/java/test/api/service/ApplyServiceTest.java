d863f3ddcab0acbc06de9802009f6e1675f1e988
koo995 <107671886+koo995@users.noreply.github.com>
2024-09-29 22:51:25 +0900

    더 많은 쿠폰이 발급되는 race condition 발생

--- api/src/test/java/test/api/service/ApplyServiceTest.java
+++ api/src/test/java/test/api/service/ApplyServiceTest.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import test.api.repository.CouponRepository;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
@@ -30,4 +34,28 @@ class ApplyServiceTest {
         assertThat(count).isEqualTo(1);
     }
 
+    @DisplayName("동시에 여러명 응모")
+    @Test
+    void 여러_응모() throws Exception {
+        // given
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
+    }
 }
